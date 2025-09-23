59ffa7e7f41a579e8669bbdc96b7140fb484a862
Charles Ofria <charles.ofria@gmail.com>
2025-09-22 21:16:18 -0400

    Added Parse functions for building an AST.

--- SimpleLang/SimpleLang.cpp
+++ SimpleLang/SimpleLang.cpp
@@ -4,53 +4,19 @@
 #include <map>
 
 #include "lexer.hpp"
+#include "ASTNode.hpp"
+#include "SymbolTable.hpp"
 
 using emplex::Lexer;
 using emplex::Token;
-
-class SymbolTable {
-private:
-  struct VarInfo {
-    int value;
-    int def_line;
-  };
-
-  std::map<std::string, VarInfo> symbols;  // From var name to value.
-public:
-  void AddSymbol(Token token) {
-    if (symbols.contains(token.lexeme)) {
-      std::cerr << "ERROR (line " << token.line_id << "): "
-        << "Redeclaration of variable '"
-        << token.lexeme << "'.\n"
-        << "Originally defined on line " << symbols[token.lexeme].def_line << ".\n";
-      exit(1);
-    }
-    symbols[token.lexeme].value = 0;
-    symbols[token.lexeme].def_line = token.line_id;
-  }
-
-  [[nodiscard]] int GetSymbolValue(std::string name) {
-    if (!symbols.contains(name)) {
-      std::cerr << "Unknown variable '" << name << "'.\n";
-      exit(1);
-    }
-    return symbols[name].value;
-  }
-
-  void SetSymbol(std::string name, int value) {
-    if (!symbols.contains(name)) {
-      std::cerr << "Unknown variable '" << name << "'.\n";
-      exit(1);
-    }
-    symbols[name].value = value;
-  }
-};
 
 class SimpleLang {
 private:
   std::string filename;
   Lexer lexer;
   SymbolTable symbols;
+
+  ASTNode root{Token{'{', "ROOT", 0}};  // Root is a block of code.
 
   int GetTokenValue(Token token) {
     if (token == Lexer::ID_ID) {
@@ -64,43 +30,105 @@ private:
     return 0;
   }
 
-  void ProcessVar() {
-    lexer.Use(Lexer::ID_VAR, "Internal Compiler Error!");
+  // void ProcessVar() {
+  //   lexer.Use(Lexer::ID_VAR, "Internal Compiler Error!");
+  //   Token id_token = lexer.Use(Lexer::ID_ID);
+  //   std::string var_name = id_token.lexeme;
+  //   symbols.AddSymbol(id_token);
+  //   lexer.Use('=');
+  //   int value = GetTokenValue(lexer.Use());
+  //   symbols.SetSymbol(var_name, value);
+  // }
+
+  // void ProcessPrint() {
+  //   lexer.Use(Lexer::ID_PRINT, "Internal Compiler Error!");
+  //   auto token = lexer.Use();
+  //   std::cout << GetTokenValue(token) << std::endl;
+  // }
+
+  // void ProcessLine() {
+  //   auto token = lexer.Peek();
+  //   switch (token) {
+  //     case Lexer::ID_VAR: ProcessVar(); break;
+  //     case Lexer::ID_PRINT: ProcessPrint(); break;
+  //     case ';': break;
+  //     default: lexer.Error("Unknown token '", token.lexeme, "'.");
+  //   }
+  //   lexer.Use(';');
+  // }
+
+  ASTNode Parse_Term() {
+    int next_token = lexer.Peek();
+    switch (next_token) {
+    case Lexer::ID_ID:
+      return ASTNode(lexer.Use());
+    case Lexer::ID_NUMBER:
+      return ASTNode(lexer.Use());
+    default:
+      lexer.Error("Unexpected Token");
+    }
+
+    // SHOULD NEVER GET HERE!
+    return ASTNode(Token{Lexer::ID__EOF_, "ERROR", 0});
+  }
+
+  ASTNode Parse_Expression() {
+    ASTNode expr_node = Parse_Term();
+
+    if (lexer.Peek() == Lexer::ID_OP) {
+      Token op_token = lexer.Use();
+      ASTNode term2 = Parse_Term();
+      expr_node = ASTNode(op_token, expr_node, term2);
+    }
+
+    return expr_node;
+  }
+
+  ASTNode Parse_StatementVAR() {
+    lexer.Use(Lexer::ID_VAR);
     Token id_token = lexer.Use(Lexer::ID_ID);
+    ASTNode lhs_node(id_token);
     std::string var_name = id_token.lexeme;
     symbols.AddSymbol(id_token);
-    lexer.Use('=');
-    int value = GetTokenValue(lexer.Use());
-    symbols.SetSymbol(var_name, value);
+    Token assign_token = lexer.Use('=');
+    ASTNode rhs_node = Parse_Expression();  // Right-hand side of assignment
+    return ASTNode(assign_token, lhs_node, rhs_node);
   }
 
-  void ProcessPrint() {
-    lexer.Use(Lexer::ID_PRINT, "Internal Compiler Error!");
-    auto token = lexer.Use();
-    std::cout << GetTokenValue(token) << std::endl;
+  ASTNode Parse_StatementPRINT() {
+    Token print_token = lexer.Use(Lexer::ID_PRINT);
+    ASTNode print_node = Parse_Expression();
+    return ASTNode(print_token, print_node);
   }
 
-  void ProcessLine() {
-    auto token = lexer.Peek();
-    switch (token) {
-      case Lexer::ID_VAR: ProcessVar(); break;
-      case Lexer::ID_PRINT: ProcessPrint(); break;
-      case ';': break;
-      default: lexer.Error("Unknown token '", token.lexeme, "'.");
+  ASTNode Parse_Statement() {
+    int next_token = lexer.Peek();
+    ASTNode statement_node;
+    switch (next_token) {
+      case Lexer::ID_VAR:   statement_node = Parse_StatementVAR();   break;
+      case Lexer::ID_PRINT: statement_node = Parse_StatementPRINT(); break;
+      default:
+        lexer.Error("Unexpected Token");
     }
+
     lexer.Use(';');
+    return statement_node;
   }
 
 public:
   SimpleLang(std::string filename) : filename(filename) { }
 
-  void Run() {
+  void Parse() {
     std::ifstream fs(filename);
     lexer.Tokenize(fs);
 
     while (lexer.Any()) {
-      ProcessLine();
+      ASTNode statement_node = Parse_Statement();
+      root.AddChild(statement_node);
     }
+
+    root.Run(symbols);
+    // root.PrintDebug();
   }
 };
 
@@ -112,5 +140,5 @@ int main(int argc, char * argv[]) {
 
   std::string filename = argv[1];
   SimpleLang lang(filename);
-  lang.Run();
+  lang.Parse();
 }
