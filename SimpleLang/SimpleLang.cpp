8f991d4faef44ae304027cf5865a3586cd2bef75
Charles Ofria <charles.ofria@gmail.com>
2025-09-30 20:10:07 -0400

    Updated SimpleLang to use a Pratt-style parser.

--- SimpleLang/SimpleLang.cpp
+++ SimpleLang/SimpleLang.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <limits>
 #include <map>
 
 #include "lexer.hpp"
@@ -12,9 +13,21 @@ using emplex::Token;
 
 class SimpleLang {
 private:
-  std::string filename;
-  Lexer lexer;
-  SymbolTable symbols;
+  std::string filename;  // Source file being compiled
+  Lexer lexer;           // Lexer to perform the tokenization
+  SymbolTable symbols;   // Current status of all variables
+
+  struct OpInfo {
+    int prec;
+    bool recurse_left = false;
+    bool recurse_right = false;
+  };
+
+  std::unordered_map<std::string, OpInfo> op_map;  // Info about binary operators
+  int max_prec_level = 0;                          // Max current precedence level
+
+  // Use max int as the precedence if we don't have an operator.
+  static constexpr int NO_OP_PREC = std::numeric_limits<int>::max();
 
   ASTNode root{Token{'{', "ROOT", 0}};  // Root is a block of code.
 
@@ -51,54 +64,93 @@ private:
     return ASTNode(Token{Lexer::ID__EOF_, "ERROR", 0});
   }
 
-  ASTNode Parse_Expr3() {
-    ASTNode lhs = Parse_Expr2();
-    return Parse_Expr3(lhs);
+  // ASTNode Parse_Expr3() {
+  //   ASTNode lhs = Parse_Expr2();
+  //   return Parse_Expr3(lhs);
+  // }
+
+  // ASTNode Parse_Expr3(ASTNode lhs) {
+  //   if (lexer.Peek().lexeme == "+" || lexer.Peek().lexeme == "-") {
+  //     Token op_token = lexer.Use(Lexer::ID_OP);
+  //     ASTNode rhs = Parse_Expr2();
+  //     lhs = ASTNode{op_token, lhs, rhs};
+
+  //     lhs = Parse_Expr3(lhs);
+  //   }
+
+  //   return lhs;
+  // }
+
+  // ASTNode Parse_Expr2() {
+  //   ASTNode lhs = Parse_Expr1();
+  //   return Parse_Expr2(lhs);
+  // }
+
+  // ASTNode Parse_Expr2(ASTNode lhs) {
+  //   if (lexer.Peek().lexeme == "*" || lexer.Peek().lexeme == "/") {
+  //     Token op_token = lexer.Use(Lexer::ID_OP);
+  //     ASTNode rhs = Parse_Expr1();
+  //     lhs = ASTNode{op_token, lhs, rhs};
+
+  //     lhs = Parse_Expr2(lhs);
+  //   }
+
+  //   return lhs;
+  // }
+  // ASTNode Parse_Expr1() {
+  //   ASTNode lhs = Parse_Term();
+
+  //   if (lexer.Peek().lexeme == "**") {
+  //     Token op_token = lexer.Use(Lexer::ID_OP);
+  //     ASTNode rhs = Parse_Expr1();
+  //     lhs = ASTNode{op_token, lhs, rhs};
+  //   }
+
+  //   return lhs;
+  // }
+
+  OpInfo GetOpInfo(Token token) {
+    auto it = op_map.find(token.lexeme);
+    if (it == op_map.end()) return OpInfo{NO_OP_PREC};
+    return it->second;
   }
 
-  ASTNode Parse_Expr3(ASTNode lhs) {
-    if (lexer.Peek().lexeme == "+" || lexer.Peek().lexeme == "-") {
-      Token op_token = lexer.Use(Lexer::ID_OP);
-      ASTNode rhs = Parse_Expr2();
-      lhs = ASTNode{op_token, lhs, rhs};
+  // ASTNode Parse_Expression() { return Parse_Expr3(); }
 
-      lhs = Parse_Expr3(lhs);
+  ASTNode Parse_Expression(int prec_level) {
+    assert(prec_level >= 0 && prec_level <= max_prec_level);
+
+    // If our precedence can't match operators, return a term.
+    if (prec_level == 0) return Parse_Term();
+
+    // Fill out any higher precedence on left-hand side (LHS)
+    ASTNode lhs = Parse_Expression(prec_level - 1);
+
+    // If we found an operator on THIS precedence level, act on it.
+    OpInfo op_info;
+    while ((op_info = GetOpInfo(lexer.Peek())).prec == prec_level) {
+      Token op_token = lexer.Use();  // Get the current operator.
+
+      // Determine the right-hand-side of this operator.
+      size_t right_prec = op_info.recurse_right ? prec_level : prec_level - 1;
+      ASTNode rhs = Parse_Expression(right_prec);
+
+      // Now that we have both sides, build the node!
+      ASTNode result = ASTNode{op_token, lhs, rhs};
+
+      // If left associative, shift result to lhs and loop to put any more
+      // ops at this precedence level ABOVE this one.
+      if (op_info.recurse_left) lhs = result;
+      else return result; // ...otherwise exit with this result.
     }
 
     return lhs;
   }
 
-  ASTNode Parse_Expr2() {
-    ASTNode lhs = Parse_Expr1();
-    return Parse_Expr2(lhs);
-  }
+  // If precedence level is not specified, use max.
+  ASTNode Parse_Expression() { return Parse_Expression(max_prec_level); }
 
-  ASTNode Parse_Expr2(ASTNode lhs) {
-    if (lexer.Peek().lexeme == "*" || lexer.Peek().lexeme == "/") {
-      Token op_token = lexer.Use(Lexer::ID_OP);
-      ASTNode rhs = Parse_Expr1();
-      lhs = ASTNode{op_token, lhs, rhs};
-
-      lhs = Parse_Expr2(lhs);
-    }
-
-    return lhs;
-  }
-  ASTNode Parse_Expr1() {
-    ASTNode lhs = Parse_Term();
-
-    if (lexer.Peek().lexeme == "**") {
-      Token op_token = lexer.Use(Lexer::ID_OP);
-      ASTNode rhs = Parse_Expr1();
-      lhs = ASTNode{op_token, lhs, rhs};
-    }
-
-    return lhs;
-  }
-
-  ASTNode Parse_Expression() { return Parse_Expr3(); }
-
-  // ASTNode Parse_Expression() {
+    // ASTNode Parse_Expression() {
   //   ASTNode expr_node = Parse_Term();
 
   //   if (lexer.Peek() == Lexer::ID_OP) {
@@ -134,7 +186,8 @@ private:
       case Lexer::ID_VAR:   statement_node = Parse_StatementVAR();   break;
       case Lexer::ID_PRINT: statement_node = Parse_StatementPRINT(); break;
       default:
-        lexer.Error("Unexpected Token: '", next_token.lexeme, "'");
+        statement_node = Parse_Expression();
+//        lexer.Error("Unexpected Token: '", next_token.lexeme, "'");
     }
 
     lexer.Use(';');
@@ -142,7 +195,12 @@ private:
   }
 
 public:
-  SimpleLang(std::string filename) : filename(filename) { }
+  SimpleLang(std::string filename) : filename(filename) {
+    op_map["**"] = OpInfo{++max_prec_level, false, true}; // Right-associative
+    op_map["*"] = op_map["/"] = OpInfo{++max_prec_level, true, false}; // Left
+    op_map["+"] = op_map["-"] = OpInfo{++max_prec_level, true, false}; // Left
+    op_map["="] = OpInfo{++max_prec_level, false, true}; // Right-associative
+  }
 
   void Parse() {
     std::ifstream fs(filename);
